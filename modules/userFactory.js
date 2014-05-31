d892307371397272a022d7c04f5561df1b7f5fe7
Andrii Shumada <eagleeyes91@gmail.com>
2014-05-31 23:47:10 +0300

    rise zombie!

--- modules/userFactory.js
+++ modules/userFactory.js
@@ -12,7 +12,7 @@
     getInfo = function() {
       var hasStarted, start, timer;
       if (user.getZombie || user.selfZombie) {
-        user.timer = moment(user.lastActionDate).add(30, 'hours').diff(moment());
+        user.timer = moment(user.lastActionDate).add(70, 'hours').diff(moment());
         user.isDead = user.timer < 0;
         user.role = 'zombie';
       } else {
@@ -32,7 +32,7 @@
           user.isDead = false;
         } else {
           user.role = 'zombie';
-          user.timer = moment(user.lastActionDate).add(60, 'hours').diff(moment());
+          user.timer = moment(user.lastActionDate).add(100, 'hours').diff(moment());
           user.isDead = user.timer < 0;
         }
       }
