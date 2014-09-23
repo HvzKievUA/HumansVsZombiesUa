9c5917e6c993fddaa4287bcb58c9a4c5c081939f
Andrii Shumada <eagleeyes91@gmail.com>
2014-09-24 00:17:30 +0300

    gulp task for sass, ignore built css

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,18 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
-var nodemon = require('gulp-nodemon')
+var nodemon = require('gulp-nodemon');
+var sass = require('gulp-sass');
 
 paths = {
 	coffeeClient: './client/js/src/*.coffee',
-	jsClient: './client/js/build/'
+	jsClient: './client/js/build/',
+	sassSrc: './client/css/src/*.scss',
+	sassSrcWatch: './client/css/src/**/*.scss',
+	cssBuild: './client/css/build/'
 };
 
-gulp.task('coffee-client', ['clean'], function() {
+gulp.task('coffee-client', ['clean-js'], function() {
 	gulp.src(paths.coffeeClient)
 		.pipe(sourcemaps.init())
 		.pipe(coffee({bare: true}).on('error', gutil.log))
@@ -18,12 +22,17 @@ gulp.task('coffee-client', ['clean'], function() {
 		.pipe(gulp.dest(paths.jsClient))
 });
 
-gulp.task('watch', ['coffee-client'], function() {
+gulp.task('watch', ['coffee-client', 'sass'], function() {
 	gulp.watch(paths.coffeeClient, ['coffee-client']);
+	gulp.watch(paths.sassSrcWatch, ['sass']);
 });
 
-gulp.task('clean', function(cb) {
+gulp.task('clean-js', function(cb) {
 	del([paths.jsClient], cb);
+});
+
+gulp.task('clean-css', function(cb) {
+	del([paths.cssBuild], cb);
 });
 
 gulp.task('nodemon', function () {
@@ -36,3 +45,12 @@ gulp.task('nodemon', function () {
 });
 
 gulp.task('dev', ['nodemon', 'watch']);
+gulp.task('build', ['coffee-client', 'sass']);
+
+gulp.task('sass', ['clean-css'], function () {
+	gulp.src(paths.sassSrc)
+		.pipe(sass({
+			sourceComments: 'map'
+		}))
+		.pipe(gulp.dest(paths.cssBuild));
+});
